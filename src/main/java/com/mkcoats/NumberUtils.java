f39bc4f7f48671817b1aabaf6109a6541b141646
M. Koby Coats <michaelkobycoats@gmail.com>
2025-12-04 20:04:27 -0800

    Adding changes from branch (#1)

--- src/main/java/com/mkcoats/NumberUtils.java
+++ src/main/java/com/mkcoats/NumberUtils.java
@@ -4,4 +4,8 @@ public class NumberUtils {
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
 }
